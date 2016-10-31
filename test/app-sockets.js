f971cc83cb0cbe58edc6a9bb27427145fd418c9e
Igor Ramadas <igor@devv.com>
2016-10-31 16:29:58 +0100

    Updated and splitted app / http tests.

--- test/app-sockets.js
+++ test/app-sockets.js
@@ -1,15 +1,17 @@
-// TEST: APP
+// TEST: APP SOCKETS
 
 require("coffee-script/register");
 var chai = require("chai");
 chai.should();
 
-describe("App Tests", function () {
+describe("App Sockets Tests", function () {
     var env = process.env;
     if (!env.NODE_ENV || env.NODE_ENV == "") env.NODE_ENV = "test";
 
     var settings = require("../lib/settings.coffee");
     settings.loadFromJson("settings.test.json");
+    settings.loadFromJson("../plugins/sockets/settings.default.json");
+    settings.app.port = 8080;
 
     var app = null;
     var sockets = null;
@@ -33,77 +35,16 @@ describe("App Tests", function () {
         sockets.expresser.logger = require("../lib/logger.coffee");
     });
 
-    it("Has app settings defined", function () {
-        settings.should.have.property("app");
-    });
-
     it("Has sockets settings defined", function () {
         settings.should.have.property("sockets");
     });
 
-    it("Init app server", function () {
+    it("Init app server with sockets, default port 8080", function () {
         this.timeout(10000);
 
         app.init();
-    });
-
-    it("Init sockets", function () {
-        this.timeout(10000);
-
         sockets.init();
         sockets.bind(app.httpServer);
-    });
-
-    it("Renders a test view", function (done) {
-        this.timeout(10000);
-
-        app.server.get("/testview", function (req, res) {
-            app.renderView(req, res, "testview.pug");
-        });
-
-        supertest(app.server).get("/testview").expect(200, done);
-    });
-
-    it("Renders a JSON object", function (done) {
-        this.timeout(10000);
-
-        app.server.get("/testjson", function (req, res) {
-            var j = {
-                "string": "some value",
-                "boolean": true,
-                "int": 123,
-                "date": new Date()
-            }
-
-            app.renderJson(req, res, j);
-        });
-
-        supertest(app.server).get("/testjson").expect("Content-Type", /json/).expect(200, done);
-    });
-
-    it("Renders an error with status 500", function (done) {
-        this.timeout(10000);
-
-        app.server.get("/testerror", function (req, res) {
-            var e = {
-                "somerror": new Error("Access was denied"),
-                "timestamp": new Date().getTime()
-            }
-
-            app.renderError(req, res, e, 500);
-        });
-
-        supertest(app.server).get("/testerror").expect("Content-Type", /json/).expect(500, done);
-    });
-
-    it("Renders a JPG image", function (done) {
-        this.timeout(10000);
-
-        app.server.get("/testjpg", function (req, res) {
-            app.renderImage(req, res, __dirname + "/testimage.jpg");
-        });
-
-        supertest(app.server).get("/testjpg").expect("Content-Type", /image/).expect(200, done);
     });
 
     it("Emits sockets message from client to server", function (done) {
