27faf17197669275c341d80664d7639d521198ff
Igor Ramadas <igor@devv.com>
2021-05-03 19:42:03 +0200

    Updated docs.

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-// Anyhow: Utils (largely based on lodash.js)
+// Expresser: Utils (largely based on lodash.js)
 
 /**
  * Get the passed object's tag.
@@ -52,14 +52,6 @@ export const isArray = (value): boolean => {
 }
 
 /**
- * Check if the passed value is same as args.
- * @param value Object or value.
- */
-export const isArguments = (value): boolean => {
-    return isObject(value) && getTag(value) == "[object Arguments]"
-}
-
-/**
  * Check if the passed value is an error.
  * @param value Object or value.
  */
@@ -81,50 +73,10 @@ export const isFunction = (value): boolean => {
 }
 
 /**
- * Check if the passed value is null or undefined.
- * @param value Object or value.
- */
-export const isNil = (value): boolean => {
-    return value === null || typeof value == "undefined"
-}
-
-/**
  * Check if the passed value is a string.
  * @param value Object or value.
  */
 export const isString = (value): boolean => {
     const type = typeof value
     return type === "string" || (type === "object" && value != null && !Array.isArray(value) && getTag(value) == "[object String]")
-}
-
-/**
- * Flatten the passed array.
- * @param value Object or value.
- */
-export const flattenArray = (array, depth?, result?): any[] => {
-    const length = array == null ? 0 : array.length
-    if (!length) return []
-
-    if (isNil(depth)) depth = 1 / 0
-    if (isNil(result)) result = []
-
-    const predicate = (value) => Array.isArray(value) || isArguments(value) || !!(value && value[Symbol.isConcatSpreadable])
-
-    if (array == null) {
-        return result
-    }
-
-    for (const value of array) {
-        if (depth > 0 && predicate(value)) {
-            if (depth > 1) {
-                flattenArray(value, depth - 1, result)
-            } else {
-                result.push(...value)
-            }
-        } else {
-            result[result.length] = value
-        }
-    }
-
-    return result
 }
